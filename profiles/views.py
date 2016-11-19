52ef60d8ba35a845fbd236d753ae1b396904f835
togrulmamedov <togrul.mamedov@hotmail.com>
2016-11-19 15:50:00 +0200

    Add django.contrib.sites

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http imoprt HttpResponse
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You are at profilse index.")
