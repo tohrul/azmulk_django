7f95bbc84cfa59ed232eeb2989ef77ec96e5baf8
togrulmamedov <togrul.mamedov@hotmail.com>
2016-12-09 19:31:13 +0200

    Edit models

--- adverts/models.py
+++ adverts/models.py
@@ -3,17 +3,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Advert(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=1023)
+    author_name = models.CharField(max_length=127)
+    phone_number = models.CharField(max_length=56)
+    price = models.BigIntegerField()
+    views = models.IntegerField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
 class Country(models.Model):
+    name = models.CharField(max_length=255)
 
 class CountryTranslation(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    language = models.CharField(max_length=56)
+    name = models.CharField(max_length=255)
 
 class District(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
 
 class DistrictTranslation(models.Model):
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    language = models.CharField(max_length=56)
+    name = models.CharField(max_length=255)
 
 class City(models.Model):
 
+    
 class CityTranslation(models.Model):
 
 class Image(models.Model):
